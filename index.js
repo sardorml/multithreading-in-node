0716e0b4b40f1b326114fd3ccfa2463a6dcff8fd
sardorml <sardormamadaliyev@gmail.com>
2024-12-10 00:52:07 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const glob = require('glob');
-const imageScale = require('./image-scale-threaded');
-
+const glob = require("glob");
+const imageScale = require("./image-scale-threaded");
+console.time("time");
 (async () => {
-  await Promise.all(glob
-    .sync('./images/*.jpeg')
-    .filter(img => img.indexOf('_small') < 0)
-    .map(imageScale));
+  await Promise.all(
+    glob
+      .sync("./images/*.jpeg")
+      .filter((img) => img.indexOf("_small") < 0)
+      .map(imageScale)
+  );
+  console.timeEnd("time");
 })();
-
